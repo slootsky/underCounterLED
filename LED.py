c592ab6b3ebfdae3eab4d27374f24295383f113b
slootsky <justin@slootsky.org>
2014-02-03 21:12:47 -0500

    v12 - backfill complete
    added debug (in a clunky manner)

--- LED.py
+++ LED.py
@@ -8,6 +8,8 @@ import time
 import datetime
 import ephem
 
+DEBUG = 1
+
 C_INIT = 0
 C_OFF = 1
 C_LOW = 2
@@ -18,7 +20,11 @@ C_NIGHT = 12
 C_DAY_START = C_OFF
 C_NIGHT_START = C_AUTO
 
+#C_MIN_LIGHT_READING = 0
 C_MIN_LIGHT_READING = 0.05
+
+
+
 
 def button_pressed(value):
 	global mode
@@ -27,29 +33,35 @@ def button_pressed(value):
 	if last_time == C_DAY :
 		if mode == C_OFF :
 			mode = C_AUTO
-			print ("mode: AUTO")
+			if DEBUG > 0 :
+				print ("mode: AUTO")
 		elif mode == C_AUTO :
 			mode = C_HIGH
-			print ("mode: HIGH")
-		elif MODE == C_HIGH :
+			if DEBUG > 0 :
+				print ("mode: HIGH")
+		elif mode == C_HIGH :
 			mode = C_OFF
-			print ("mode: OFF")
+			if DEBUG > 0 :
+				print ("mode: OFF")
 
 	elif last_time == C_NIGHT :
 		if mode == C_LOW :
 			mode = C_AUTO
-			print ("mode: AUTO")
+			if DEBUG > 0 :
+				print ("mode: AUTO")
 		elif mode == C_AUTO :
 			mode = C_HIGH
-			print ("mode: HIGH")
+			if DEBUG > 0 :
+				print ("mode: HIGH")
 		elif mode == C_HIGH :
 			mode = C_LOW
-			print ("mode: LOW")
+			if DEBUG > 0 :
+				print ("mode: LOW")
  
-red = "P8_13"
-green = "P8_19"
+green = "P8_13"
+red = "P8_19"
 blue = "P9_14"
-light_sensor = "P9_40"
+light_sensor = "AIN0" # "P9_40"
 button = "P8_11"
  
 night_red=float(100)
@@ -60,10 +72,11 @@ day_red=float(100)
 day_green=float(75)
 day_blue=float(55)
 
+ADC.setup()
+
 PWM.start(red, 0)
 PWM.start(blue, 0)
 PWM.start(green, 0)
-ADC.setup()
 
 last_light_reading = -1
 mode = C_INIT
@@ -71,6 +84,7 @@ last_time = C_INIT
 
 GPIO.setup ( button, GPIO.IN )
 GPIO.add_event_detect(button, GPIO.RISING, callback=button_pressed, bouncetime=300)
+
  
 while True:
 	o=ephem.Observer()  
@@ -93,8 +107,11 @@ while True:
 		light_reading = ADC.read(light_sensor)
 
 		# exaggerate the light reading (0-1)
-		light_reading = math.log10(light_reading*100) - 1
-		light_reading = light_reading * light_reading * light_reading
+		light_reading = 1.0 - (light_reading + light_reading)
+#		light_reading = math.log10(light_reading*100) - 1
+#		light_reading = light_reading * light_reading * light_reading
+#		light_reading = light_reading * light_reading * light_reading * light_reading
+#		light_reading = light_reading * light_reading * light_reading * light_reading * light_reading
 #		light_reading = light_reading * light_reading 
 
 		if ( light_reading > 1) :
@@ -106,6 +123,10 @@ while True:
 			last_light_reading = light_reading *2
 
 #		light_change = abs( ( last_light_reading - light_reading ) / light_reading)
+
+		if DEBUG > 1 :
+			print "light_reading: {}".format(light_reading)
+
 		if (next_sunrise < next_sunset):
 			#
 			# NIGHT TIME
@@ -116,7 +137,7 @@ while True:
 
 #                       print "currently night"
 #                       next_event=next_sunrise
-#                       print "light: {} \t red: {}".format(light_reading,night_red*light_reading)
+
 #                       if ( light_change > .1) or ( last_time == C_DAY ) :
 
 			if mode == C_INIT :
@@ -128,6 +149,9 @@ while True:
 				multiplier = 1
 			else :
 				multiplier = light_reading
+
+			if DEBUG > 1 :
+				print "light: {} \t muliplier: {} \t red: {}".format(light_reading,multiplier, night_red*multiplier)
 
 			PWM.set_duty_cycle(red,night_red*multiplier)
 			PWM.set_duty_cycle(green,night_green*multiplier)
@@ -155,12 +179,15 @@ while True:
 			else :
 				multiplier = light_reading
 
+			if DEBUG > 1 :
+				print "light: {} \t muliplier: {} \t red: {}".format(light_reading,multiplier, night_red*multiplier)
+
 			PWM.set_duty_cycle(red,day_red*multiplier)
 			PWM.set_duty_cycle(green,day_green*multiplier)
 			PWM.set_duty_cycle(blue,day_blue*multiplier)
 
 		# sleep for about 50ms
-#		time.sleep(50.0/1000.0)
+		time.sleep(50.0/1000.0)
 		now=datetime.datetime.now()
 
 
